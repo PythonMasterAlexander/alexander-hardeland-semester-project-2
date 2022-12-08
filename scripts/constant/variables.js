11c5c9589fe9a6c82da3579dc3f255b8818c0d4f
alexander <alexanderhardeland@gmail.com>
2022-12-08 17:57:59 +0100

    Started to work on a better and new error function. With some more input

--- scripts/constant/variables.js
+++ scripts/constant/variables.js
@@ -1,1 +1,2 @@
 export const indexPageHtmlContainer = document.querySelector(".products-container");
+export const containerForErrorMessage = document.querySelector(".container-for-error-message");
--- scripts/utilities/errorMessage.js
+++ scripts/utilities/errorMessage.js
@@ -1,10 +1,24 @@
 import createHtmlElement from '../components/createHtmlElement.js';
 
+/*
 const errorMessage = function(containerElement, errorMessage, element) {
   containerElement.classList.add("alert-warning", "alert");
   containerElement.setAttribute("role", "alert");
 
   const errorMessageInHtml = createHtmlElement(containerElement, errorMessage, element);
   return errorMessageInHtml;
-}
+};
+*/
+
+//Writing the new error function
+const errorMessage = function(containerElement, errorMessageToUser = "There was a problem loading the content") {
+  if (containerElement) {
+    const errorMessageStyles = {
+      alert: "alert",
+      warning: "alert-warning",
+      attribute: "role",
+      aria: "alert"
+    }
+  }
+};
 export default errorMessage;
