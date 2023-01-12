3f6f491fcf9fff0a2424f45b5cbce3ef30994c83
alexander <alexanderhardeland@gmail.com>
2023-01-12 16:34:38 +0100

    Continue working on admin JS

--- scripts/components/submitProductToApi.js
+++ scripts/components/submitProductToApi.js
@@ -1,4 +1,7 @@
+import { baseUrl, productsUrl } from '../constant/constants.js';
 import { addProductsForm } from '../constant/variables.js';
+
+const url = baseUrl + productsUrl;
 
 export default function submitProductToApi() {
   event.preventDefault();
@@ -10,28 +13,35 @@ export default function submitProductToApi() {
   //Check if price is NaN
 
   const description = formProperties.get("description");
-  description.trim();
 
   const image = formProperties.get("image");
 
-  if ((title === 0) || (isNaN(price)) || (description === 0)) {
+  if ((title <= 0) || (isNaN(price)) || (description <= 0)) {
     //Create a warning message if any of the fields are not populated, use a return statement too
-    console.log(typeof(price));
+    console.log("not working" + isNaN(price));
+  } else {
+    console.log("working" + isNaN(price));
   }
+
+
   //Use the add product function
   //addProductToApi(title, price, description, image, url);
 }
 
-function addProductToApi(title, price, description, image, url) {
+async function addProductToApi(title, price, description, image, url) {
   const apiUrl = url;
   let apiToken;
+  let outputError;
+  let apiData;
 
-  const dataToSendToApi = JSON.stringify({
-    title: title,
-    price: price,
-    description: description,
-    image: image
-  });
+  const dataToSendToApi = JSON.stringify(
+    {
+      title: title,
+      price: price,
+      description: description,
+      image: image
+    }
+  );
 
   const dataHeadersForLoginRequest = {
     method: "POST",
@@ -41,9 +51,30 @@ function addProductToApi(title, price, description, image, url) {
       Authorization: `Bearer ${apiToken}` 
     },
   };
+  
+  if (!apiUrl) {
+    return outputError = "Wrong api address provided";
+  } else {
 
-  try {
-  }
-  catch(error) {
+    try {
+      const response = await fetch(apiUrl, dataToSendToApi);
+      if (response.ok) {
+        apiData = await response.json();
+
+      } else {
+        outputError = `Unresponsive API call: Status: ${response.status}`;
+      }
+    }
+    catch(error) {
+      outputError = error;
+    }
+    return { outputError, apiData };
   }
 }
+
+
+
+
+
+
+
--- scripts/utilities/logoutUser.js
+++ scripts/utilities/logoutUser.js
@@ -1,5 +1,4 @@
 import { logoutButton } from '../constant/variables.js';
-console.log(logoutButton);
 
 export default function logoutUser() {
 }
