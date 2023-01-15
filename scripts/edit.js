7bdeb80d5a24499ea4c9cf8d8b8784f59a51dcb5
alexander <alexanderhardeland@gmail.com>
2023-01-15 12:19:33 +0100

    Made comments for further progress later. Now I am going to work on the cart page functionality

--- scripts/edit.js
+++ scripts/edit.js
@@ -8,13 +8,19 @@ addPageMenu();
 const searchContentInQueryString = new URLSearchParams(contentInQueryString);
 const getProductIdFromQueryString = searchContentInQueryString.get(idIdentifier);
 
-const productId = url + "/" + getProductIdFromQueryString;  
-console.log(productId);
+//const productId = url + "/" + getProductIdFromQueryString;  
+
+//To to this we need to get the products that are in the cart page
+//Show the products that are in the cart page on the admin page. 
+//When user go to edit page, the user must be able to edit the products here
+//Get product that is in the api, then load the product into edit form
+
 
 (async function(){
   try {
     const response = await fetch(url);
     const returnJson = await response.json();
+    console.log(returnJson);
 
   } catch(error) {
     console.log(error);
@@ -22,5 +28,6 @@ console.log(productId);
   } finally {
     addEditForm.style.display = "none";
     //addEditForm.style.display = "block";
+    //addEditForm.style.display = "none";
   }
 })();
