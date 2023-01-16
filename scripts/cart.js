6583e03ced75129a06318b661dc6cf80ee4a179d
alexander <alexanderhardeland@gmail.com>
2023-01-16 17:14:03 +0100

    Got stuck on some code

--- scripts/cart.js
+++ scripts/cart.js
@@ -2,38 +2,39 @@ import outputCardOnPage from './components/outputCardOnPage.js';
 const cardContainerElement = document.querySelector(".card-container");
 import getProductsFromApiCall from './components/getProductsFromApiCall.js';
 const url = "http://localhost:1337/products";
-const getApiData = await getProductsFromApiCall(url);
 const filterInputField = document.querySelector("#filter-input-field");
+
+const getApiData = await getProductsFromApiCall(url);
+let productsToFilter = getApiData.apiData;
+
 
 function outputProductsOnPage(products) {
   const listOfProducts = products.apiData;
+  //cardContainerElement.innerHTML = "";
 
-  cardContainerElement.innerHTML = "";
-
+  //Something is happening here 
   listOfProducts.forEach(function(products) {
     const { title, price, imageUrl } = products;
-
     return cardContainerElement.append(outputCardOnPage(title, price, "", "/"));
   });
 }
-
-let showProductIfFilterSearchMatch = outputProductsOnPage(getApiData);
-
-console.log(showProductIfFilterSearchMatch);
 
 outputProductsOnPage(getApiData);
 
 filterInputField.addEventListener("keyup", function() {
   const filterKeyValue = event.target.value.trim().toLowerCase();
-  const apiData = getApiData.apiData;
+  //const apiData = getApiData.apiData;
 
-  const filteredProducts = apiData.filter(function(data) {
+  const filteredProducts = productsToFilter.filter(function(data) {
     const { title } = data;
     if (title.toLowerCase().startsWith(filterKeyValue)) {
       return true;
     } 
   });
 
-  showProductIfFilterSearchMatch = filteredProducts; 
-  outputProductsOnPage(getApiData);
+  console.log(filteredProducts);
+  console.log(productsToFilter);
+  productsToFilter = filteredProducts;
+  //This is where the code don't work, the error is listOfProducts is undefinded
+  outputProductsOnPage(productsToFilter);
 });
--- scripts/script.js
+++ scripts/script.js
@@ -9,3 +9,18 @@ const waitForProductsResult = getProductsFromApiCall(url);
 
 waitForProductsResult.then(showProductsOnPage);
 
+
+
+
+
+/*
+return cardContainerElement.innerHTML += `<div class="card">
+                                            <img src"${products.image_url}">
+                                            <hgroup class="card-body">
+                                              <h2>${products.title}</h2>
+                                              <p>${products.price}</p>
+                                              <a href="product.html" class="btn btn-primary">to product</a>
+                                            </hgroup>
+                                          </div>`;
+*/
+
