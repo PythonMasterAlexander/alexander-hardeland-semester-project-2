fe7dda8458f6181f3c618526bcdeba26ff8cb45f
alexander <alexanderhardeland@gmail.com>
2023-01-17 07:08:38 +0100

    Added todo comments and two JS pages needed to create the code

---
+++ scripts/cart.js
@@ -0,0 +1,3 @@
+//This page must include a 
+//List of all products added to the cart with the local storage and then display on the cart page
+//if cart is empty display a message
---
+++ scripts/product.js
@@ -0,0 +1,4 @@
+//From this page I must be able to
+//show title description image price and a add to cart button
+//The button must toggle in and out of cart page
+
