71023189fc0d9715020b197ca0bae41b08f21132
alexander <alexanderhardeland@gmail.com>
2023-01-20 20:00:02 +0100

    Another test

--- scripts/utilities/showHeroBanner.js
+++ scripts/utilities/showHeroBanner.js
@@ -1,11 +1,9 @@
 function showHeroBanner(heroBannerUrl, heroBannerContainer) {
-  heroBannerUrl.forEach((banner) => {
-    if (!banner) {
-      //Some default value if no hero banner url
-    } else {
-      console.log(heroBannerContainer.style);
-      heroBannerContainer.style.backgroundImage = `url(${banner})`;
-    }
-  });
+  if (!heroBannerUrl) {
+    //Some default value if no hero banner url
+  } else {
+    console.log(heroBannerContainer.style);
+    heroBannerContainer.style.backgroundImage = `url(${heroBannerUrl})`;
+  }
 }
 export default showHeroBanner;
