8c33d553fd98817ba06aac2689cfd851cd1a6310
alexander <alexanderhardeland@gmail.com>
2023-02-06 19:04:20 +0100

    Started to work on changing api products

--- scripts/edit.js
+++ scripts/edit.js
@@ -12,25 +12,16 @@ addPageMenu();
 
 //To to this we need to get the products that are in the cart page
 const productsInLocalStorage = getValueFromLocalStorage(productKey);
-//const valueOfProductInLocalStorage = productsInLocalStorage.values; 
 
 const valuesOfProductsInLocalStorage = productsInLocalStorage[0].values; 
-outputProductsEditFieldsOnEditPage(valuesOfProductsInLocalStorage, editProductsContainer); 
-//Show the products that are in the cart page on the admin page. 
-//When user go to edit page, the user must be able to edit the products here
-//Get product that is in the api, then load the product into edit form
+const { title, description, price, image_url } = valuesOfProductsInLocalStorage;
 
+const titleInput = document.querySelector("#title");
+const descriptionInput = document.querySelector("#description");
+const priceInput = document.querySelector("#price");
+const imageInput = document.querySelector("#upload-image");
 
-(async function(){
-  try {
-    const response = await fetch(url);
-    const returnJson = await response.json();
-
-  } catch(error) {
-
-  } finally {
-    //addEditForm.style.display = "none";
-    //addEditForm.style.display = "block";
-    //addEditForm.style.display = "none";
-  }
-})();
+titleInput.value = title;
+priceInput.value = price;
+descriptionInput.value = description; 
+imageInput.value = image_url;
